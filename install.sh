598580962104a2e8124a17fc0153ad7f9c547e1e
matoruru <matoruru@shgt.jp>
2018-10-04 00:43:21 +0900

    add install message

--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@
 
 sudo cp dic.rb /usr/local/bin/dic
 sudo chmod 755 /usr/local/bin/dic
+
+if [ -f /usr/local/bin/dic ]; then
+   echo "succeed to install dic!"
+else
+   echo "failure to install dic..."
+fi
